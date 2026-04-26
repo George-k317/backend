2f8d1f7ea439bc3229efe63c58a3b07dfc6cda50
George-k317 <kahush56g@gmail.com>
2026-04-26 21:09:35 +0300

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -106,3 +106,4 @@ app.use((req, res) => res.status(404).json({ msg: "Not Found" }));
 
 const PORT = parseInt(process.env.PORT || "5000", 10);
 app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
